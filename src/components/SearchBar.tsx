3ece41bb96a5fd7f4bf9e0969578911b844c381a
Dale <Danntabachoyy@gmail.com>
2024-10-30 21:00:52 +0800

    fixing searchBar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,6 +1,13 @@
+import { useState } from 'react';
 import { IoMdSearch } from 'react-icons/io';
+import useCurrentWeather from '../hooks/useCurrentWeather';
 
 const SearchBar = () => {
+  const [location, setLocation] = useState('');
+  const { getCurrentWeather } = useCurrentWeather(); //! this cause to fetch again without any location
+  const handleClickSearch = () => {
+    getCurrentWeather(location);
+  };
   return (
     <div className="w-full flex justify-center">
       <div className="flex space-x-2 w-3/4">
@@ -8,8 +15,13 @@ const SearchBar = () => {
           type="text"
           className="p-3 bg-[#EBFDFD] outline-none rounded-2xl flex-1"
           placeholder="Search..."
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
         />
-        <button className="p-4 rounded-full bg-[#EBFDFD]">
+        <button
+          className="p-4 rounded-full bg-[#EBFDFD]"
+          onClick={handleClickSearch}
+        >
           <IoMdSearch />
         </button>
       </div>
--- src/hooks/useCurrentWeather.tsx
+++ src/hooks/useCurrentWeather.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { weatherApi, weatherGeoApi } from '../api/configAxios';
 import { TWeatherData } from '../types';
 
-export default function useCurrentWeather(q: string) {
+export default function useCurrentWeather(q?: string) {
   const [data, setData] = useState<TWeatherData | undefined>(undefined);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
@@ -10,59 +10,58 @@ export default function useCurrentWeather(q: string) {
   // flow
   // 1. get the latitude and longitude using a state/country q search.
   // 2. if there is a corresponding latitude and longitude then use it to get current weather data.
+  const getCurrentWeather = async (q: string | undefined) => {
+    const appid = import.meta.env.VITE_APPID;
+    setLoading(true);
+    try {
+      // send a request for the given location
+      const res = await weatherGeoApi.get('', {
+        params: {
+          q,
+          appid,
+        },
+      });
 
-  useEffect(() => {
-    const getCurrentWeather = async () => {
-      const appid = import.meta.env.VITE_APPID;
-      setLoading(true);
-      try {
-        // send a request for the given location
-        const res = await weatherGeoApi.get('', {
-          params: {
-            q,
-            appid,
-          },
-        });
+      // if there is no data
+      if (Array.from(res.data).length < 1) {
+        const error = new Error('No Location corresponds with the query');
+        error.name = 'NotFound';
+        throw error;
+      }
 
-        // if there is no data
-        if (Array.from(res.data).length < 1) {
-          const error = new Error('No Location corresponds with the query');
-          error.name = 'NotFound';
+      // if there is a lat and lon
+      const { lat, lon } = res.data[0];
+      // send a request to get the current weather data
+      const resWeatherData = await weatherApi.get('', {
+        params: {
+          lat,
+          lon,
+          appid,
+          units: 'metric',
+        },
+      });
+
+      if (resWeatherData.status === 200) {
+        // if no data recieves
+        if (
+          !resWeatherData.data &&
+          Object.keys(resWeatherData.data).length === 0
+        ) {
+          const error = new Error('No data return');
+          error.name = 'NoData';
           throw error;
         }
-
-        // if there is a lat and lon
-        const { lat, lon } = res.data[0];
-        // send a request to get the current weather data
-        const resWeatherData = await weatherApi.get('', {
-          params: {
-            lat,
-            lon,
-            appid,
-            units: 'metric',
-          },
-        });
-
-        if (resWeatherData.status === 200) {
-          // if no data recieves
-          if (
-            !resWeatherData.data &&
-            Object.keys(resWeatherData.data).length === 0
-          ) {
-            const error = new Error('No data return');
-            error.name = 'NoData';
-            throw error;
-          }
-          setData(resWeatherData.data);
-        }
-      } catch (error) {
-        setError('something went wrong');
-      } finally {
-        setLoading(false);
+        setData(resWeatherData.data);
       }
-    };
-    getCurrentWeather();
+    } catch (error) {
+      setError('something went wrong');
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    getCurrentWeather(q);
   }, [q]);
 
-  return { data, loading, error };
+  return { data, loading, error, getCurrentWeather };
 }
